5a88b42113da9da57f08653ffa7dfe7381f761ac
Adrian Istrate <adr_istrate@yahoo.com>
2013-12-05 13:15:00 +0100

    Comment out "git push" line in practice.sh.

--- practice.sh
+++ practice.sh
@@ -12,9 +12,9 @@ git commit -am '(practice)'
 # To set the default as in Git 2.0, run:
 #     git config --global push.default simple
 
-echo
-echo Push:
-git push origin master
+# echo
+# echo Push:
+# git push origin master
 
 echo
 echo Status After:
